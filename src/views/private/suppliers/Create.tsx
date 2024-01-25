e4171e7cc491617b695946032ebd1bd17a984079
cagatayyildiz <yildiz.cagatay@hotmail.com>
2024-01-25 14:51:46 +0300

    reacrt hook form sample

--- src/views/private/suppliers/Create.tsx
+++ src/views/private/suppliers/Create.tsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { axiosInstance } from '../../../network/axiosInstance'
+
+
+type SupplierFormInputs = {
+  companyName: string,
+  contactName: string,
+  contactTitle: string
+}
+
 
 function Create() {
-  return (
-    <div>Create</div>
-  )
+
+  const { register, handleSubmit } = useForm<SupplierFormInputs>()
+
+  const addSupplier = (data: SupplierFormInputs) => {
+
+    axiosInstance.post('suppliers', data)
+      .then(res => {
+        console.log("Response", res)
+      })
+      .catch(err => {
+        console.log("Error", err)
+      })
+
+  }
+
+  return (<>
+    <form onSubmit={handleSubmit(addSupplier)}>
+      <div>
+        <label>Company Name</label>
+        <input {...register('companyName')} />
+      </div>
+      <div>
+        <label>Contact Name</label>
+        <input {...register('contactName')} />
+      </div>
+      <div>
+        <label>Contact Title</label>
+        <input {...register('contactTitle')} />
+      </div>
+      <div>
+        <button type='submit'>Add</button>
+        {/* <input type='submit' value='Add' /> */}
+      </div>
+
+    </form>
+
+  </>)
 }
 
 export default Create
